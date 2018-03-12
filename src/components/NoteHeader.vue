95782efcd2c65ee5847850c19d71e5ce08d4ddb5
HiroshiOyamada <h5y1m141@gmail.com>
2018-03-12 18:58:05 +0900

    本文のプレビュー含めた処理を修正

---
+++ src/components/NoteHeader.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="page-NoteEdit-header">
+  <input aria-label="タイトル" ref="title" type="text" :value="note.title" data-page-title />
+    <div class="page-NoteEdit-buttons">
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'noteheader',
+  props: {
+    note: Object
+  }
+}
+</script>
+
+<style>
+.page-NoteEdit {
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 45px);
+  overflow: hidden;
+}
+
+.page-NoteEdit-header {
+  position: relative;
+  border-bottom: 1px solid #CCC;
+  padding: 5px;
+  height: 50px;
+  box-sizing: border-box;
+}
+
+.page-NoteEdit-header > input[type="text"] {
+  font-size: 24px;
+  padding: 5px;
+  border: none;
+  font-weight: bold;
+  width: calc(100% - 200px);
+  outline: none;
+}
+
+.page-NoteEdit-buttons {
+  position: absolute;
+  right: 0;
+  top: 10px;
+}
+</style>
